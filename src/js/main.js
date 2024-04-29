5bd530909b66756cc3aa62c90668385438eee37c
PixelPlysha <ibif3485@gmail.com>
2024-04-29 10:10:03 -0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+import '/scss/main.scss';
+
+// components
+
+
+import { useBurger } from './components/burger.js';
+import {
+  useInsightSlider,
+  useTestimonialsSlider,
+} from './components/home/slider.js';
+
+
+
+useBurger();
+useInsightSlider();
+useTestimonialsSlider();
---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
+export default defineConfig({
+  root: './src',
+  base: '/bundle/',
+  server: {
+    port: 3000,
+    open: true,
+  },
+  publicDir: './src/assets',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+      },
+    },
+  },
+  plugins: [
+    injectHTML(),
+    ViteMinifyPlugin(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+    }),
+  ],
+});
