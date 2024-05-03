a7b22c8972f75ba1a783b17cd90eb8f088fb0a0a
PixelPlysha <ibif3485@gmail.com>
2024-05-03 17:54:49 -0300

    proces-section popup fixed-slider

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,12 @@ import { useBurger } from './components/burger.js';
 import { useVisualSlider } from './components/slider.js';
 import { useNum } from './components/num.js';
 import { useAccordion } from './components/accordion.js';
+import { usePopUp } from './components/popup.js';
+import { usePhone } from './components/phone.js';
 
 useBurger();
 useVisualSlider();
 useNum();
 useAccordion();
+usePopUp();
+usePhone();
