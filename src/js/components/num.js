006f28c898c5128654ec670572e9a07dddf4e21b
PixelPlysha <ibif3485@gmail.com>
2024-05-10 10:20:22 -0300

    fixed-num

--- src/js/components/num.js
+++ src/js/components/num.js
@@ -1,7 +1,8 @@
+
 export const useNum = () => {
-  const time = 1000; // ms
+  const time = 1000; 
   const step = 1;
-  const targetNum = 20; // Целевое значение
+  const targetNum = 20;
 
   function outNum(elem) {
     let n = 0;
@@ -17,6 +18,34 @@ export const useNum = () => {
     }, t);
   }
 
-  // Вызов функции outNum после инициализации
-  outNum('out-1');
+  function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  let isCounterRunning = false;
+
+  function handleScroll() {
+    const section = document.querySelector('.baner');
+    const numElement = document.getElementById('out-1');
+
+    if (isInViewport(section)) {
+      if (!isCounterRunning) {
+        outNum('out-1');
+        isCounterRunning = true;
+      }
+    } else {
+      numElement.textContent = '0';
+      isCounterRunning = false;
+    }
+  }
+
+  window.addEventListener('scroll', handleScroll);
+  handleScroll();
 };
